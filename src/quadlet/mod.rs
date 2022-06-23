88bf6ecc91a339181819dd623e627ad9f6ecf5e5
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2022-06-23 21:44:58 +0200

    Add quadlet::is_port_range()

--- src/quadlet/mod.rs
+++ src/quadlet/mod.rs
@@ -1,3 +1,11 @@
 mod podman_command;
 
 pub(crate) use self::podman_command::*;
+
+use once_cell::unsync::Lazy;
+use regex::Regex;
+
+pub(crate) fn is_port_range(port: &str) -> bool {
+    const RE: Lazy<Regex> = Lazy::new(|| Regex::new("\\d+(-\\d+)?(/udp|/tcp)?$").unwrap());
+    RE.is_match(port)
+}
