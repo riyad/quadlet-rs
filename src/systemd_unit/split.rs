f88133bca0eb8e499b98468137176193463c33c2
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2022-08-07 23:42:53 +0200

    Update escaping and unquoting tests
    - reactivate tests ignored, when using rust-ini
    - add proper unescaping to `SplitWord` (previously this clashed with unescaping done by rust-ini)

--- src/systemd_unit/split.rs
+++ src/systemd_unit/split.rs
@@ -125,15 +125,30 @@ impl<'a> SplitWord<'a> {
         self.parse_until_none_of(separators);
 
         let mut quote: Option<char> = None;  // None or Some('\'') or Some('"')
+        let mut backslash = false;  // whether we've just seen a backslash
         while let Some(c) = self.c {
-            if let Some(q) = quote {
+            if backslash {
+                match self.c {
+                    // eat up trailing backslash
+                    None => break,
+                    _ => (),
+                }
+
+                match self.parse_escape_sequence() {
+                    Ok(r) => word.push(r),
+                    Err(_) => return None,
+                };
+
+                backslash = false;
+            } else if let Some(q) = quote {
                 // inside either single or double quotes
-                word.push_str(self.parse_until_any_of(&[q]).as_str());
+                word.push_str(self.parse_until_any_of(&[q, '\\']).as_str());
 
                 match self.c {
                     Some(c) if c == q => {
-                        quote = None
+                        quote = None;
                     },
+                    Some('\\') => backslash = true,
                     _ => (),
                 }
             } else {
@@ -141,9 +156,12 @@ impl<'a> SplitWord<'a> {
                     '\'' | '"' => {
                         quote = Some(c)
                     },
+                    '\\' => {
+                        backslash = true;
+                    }
                     _ if separators.contains(&c) => {
                         // word is done
-                        break
+                        break;
                     },
                     _ => word.push(c),
                 }
@@ -156,6 +174,78 @@ impl<'a> SplitWord<'a> {
             None
         } else {
             Some(word)
+        }
+    }
+
+    fn parse_escape_sequence(&mut self) -> Result<char, String> {
+        if let Some(c) = self.c {
+            let r = match c {
+                'a'  => '\u{7}',
+                'b'  => '\u{8}',
+                'f'  => '\u{c}',
+                'n'  => '\n',
+                'r'  => '\r',
+                't'  => '\t',
+                'v'  => '\u{b}',
+                '\\' => '\\',
+                '"'  => '"',
+                '\'' => '\'',
+                's'  => ' ',
+
+                'x'  => {  // 2 character hex encoding
+                    self.bump();
+                    self.parse_unicode_escape(Some('x'), 2, 16)?
+                },
+                'u'  => {  // 4 character hex encoding
+                    self.bump();
+                    self.parse_unicode_escape(Some('u'), 4, 16)?
+                },
+                'U'  => {  // 8 character hex encoding
+                    self.bump();
+                    self.parse_unicode_escape(Some('U'), 8, 16)?
+                },
+                '0'..='7' => {  // 3 character octal encoding
+                    self.parse_unicode_escape(None, 3, 8)?
+                }
+                c => c
+            };
+
+            Ok(r)
+        } else {
+            return Err("expecting escape sequence, but found EOF.".into())
+        }
+    }
+
+    fn parse_unicode_escape(&mut self, prefix: Option<char>, max_chars: usize, radix: u32) -> Result<char, String> {
+        assert!(prefix.is_none() || (prefix.is_some() && ['x', 'u', 'U'].contains(&prefix.unwrap())));
+        assert!([8, 16].contains(&radix));
+
+        let mut code = String::with_capacity(max_chars);
+        for _ in 0..max_chars {
+            if let Some(c) = self.c {
+                code.push(c);
+                if radix == 16 && !c.is_ascii_hexdigit() {
+                    return Err(format!("Expected {max_chars} hex values after \"\\{c}\", but got \"\\{c}{code}\"" ))
+                } else if radix == 8 && (!c.is_ascii_digit() || c == '8' || c == '9') {
+                    return Err(format!("Expected {max_chars} octal values after \"\\\", but got \"\\{code}\"" ))
+                }
+            } else {
+                return Err("expecting unicode escape sequence, but found EOF.".into())
+            }
+
+            if code.len() != max_chars {
+                self.bump();
+            }
+        }
+
+        let ucp = u32::from_str_radix(code.as_str(), radix).unwrap();
+        if ucp == 0 {
+            return Err("\\0 character not allowed in escape sequence".into())
+        }
+
+        return match char::try_from(ucp) {
+            Ok(u) => Ok(u),
+            Err(e) => Err(format!("invalid unicode character in escape sequence: {e}")),
         }
     }
 
@@ -406,10 +496,29 @@ mod tests {
 
             #[test]
             fn unmatched_quote_matches_till_end_of_line() {
-                let input = "\tfoo=\'bar \tbar=\"baz\"\r\n";
+                let input = "\tfoo=\'bar \tbar=\"baz\"";
 
                 let mut split = SplitWord::new(input);
-                assert_eq!(split.next(), Some("foo=bar \tbar=\"baz\"\r\n".into()));
+                assert_eq!(split.next(), Some("foo=bar \tbar=\"baz\"".into()));
+                assert_eq!(split.next(), None);
+            }
+
+            #[test]
+            fn unescapes_escape_sequences() {
+                let input = "\\tfoo\\u1234";
+
+                let mut split = SplitWord::new(input);
+                assert_eq!(split.next(), Some("\tfoo\u{1234}".into()));
+                assert_eq!(split.next(), None);
+            }
+
+            #[test]
+            fn keeps_escaped_whitespace() {
+                let input = "\\tfoo bar\\tbaz\\n";
+
+                let mut split = SplitWord::new(input);
+                assert_eq!(split.next(), Some("\tfoo".into()));
+                assert_eq!(split.next(), Some("bar\tbaz\n".into()));
                 assert_eq!(split.next(), None);
             }
         }
