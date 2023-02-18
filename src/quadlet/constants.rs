5e2201e46f5fa8d4a131c4e9fd90d3724e941715
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2023-02-18 23:16:21 +0100

    Sort constants

--- src/quadlet/constants.rs
+++ src/quadlet/constants.rs
@@ -1,52 +1,51 @@
 use once_cell::sync::Lazy;
 use std::collections::HashSet;
 
-pub const CONTAINER_SECTION: &str = "Container";
+pub const CONTAINER_SECTION: &str   = "Container";
+pub const KUBE_SECTION: &str        = "Kube";
+pub const NETWORK_SECTION: &str     = "Network";
+pub const VOLUME_SECTION: &str      = "Volume";
 pub const X_CONTAINER_SECTION: &str = "X-Container";
-pub const KUBE_SECTION: &str = "Kube";
-pub const X_KUBE_SECTION: &str = "X-Kube";
-pub const NETWORK_SECTION: &str = "Network";
-pub const X_NETWORK_SECTION: &str = "X-Network";
-pub const VOLUME_SECTION: &str = "Volume";
-pub const X_VOLUME_SECTION: &str = "X-Volume";
+pub const X_KUBE_SECTION: &str      = "X-Kube";
+pub const X_NETWORK_SECTION: &str   = "X-Network";
+pub const X_VOLUME_SECTION: &str    = "X-Volume";
 
 pub static SUPPORTED_CONTAINER_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
     let keys = [
+        "AddCapability",
+        "AddDevice",
+        "Annotation",
         "ContainerName",
-        "Image",
-        "KillMode",
+        "DropCapability",
         "Environment",
         "EnvironmentFile",
         "EnvironmentHost",
         "Exec",
-        "NoNewPrivileges",
-        "DropCapability",
-        "AddCapability",
-        "ReadOnly",
-        "RemapUsers",
-        "RemapUid",
-        "RemapGid",
-        "RemapUidSize",
-        "Rootfs",
-        "Notify",
         "ExposeHostPort",
-        "PublishPort",
-        "User",
         "Group",
-        "Volume",
-        "PodmanArgs",
+        "Image",
         "Label",
-        "Annotation",
+        "Network",
+        "NoNewPrivileges",
+        "Notify",
+        "PodmanArgs",
+        "PublishPort",
+        "ReadOnly",
+        "RemapGid",
+        "RemapUid",
+        "RemapUidSize",
+        "RemapUsers",
+        "Rootfs",
         "RunInit",
-        "VolatileTmp",
-        "Timezone",
         "SeccompProfile",
         "SecurityLabelDisable",
         "SecurityLabelFileType",
-        "SecurityLabelType",
         "SecurityLabelLevel",
-        "AddDevice",
-        "Network",
+        "SecurityLabelType",
+        "Timezone",
+        "User",
+        "VolatileTmp",
+        "Volume",
     ];
 
     let mut set = HashSet::with_capacity(keys.len());
@@ -58,15 +57,14 @@ pub static SUPPORTED_CONTAINER_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| 
 
 pub static SUPPORTED_KUBE_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
     let keys = [
-        "Yaml",
-        "KillMode",
-        "RemapUsers",
-        "RemapUid",
-        "RemapGid",
-        "RemapUidSize",
-        "Network",
         "ConfigMap",
+        "Network",
         "PublishPort",
+        "RemapGid",
+        "RemapUid",
+        "RemapUidSize",
+        "RemapUsers",
+        "Yaml",
     ];
 
     let mut set = HashSet::with_capacity(keys.len());
@@ -82,12 +80,12 @@ pub static SUPPORTED_NETWORK_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
         "Driver",
         "Gateway",
         "Internal",
-        "IPRange",
         "IPAMDriver",
+        "IPRange",
         "IPv6",
+        "Label",
         "Options",
         "Subnet",
-        "Label",
     ];
 
     let mut set = HashSet::with_capacity(keys.len());
@@ -99,13 +97,13 @@ pub static SUPPORTED_NETWORK_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
 
 pub static SUPPORTED_VOLUME_KEYS: Lazy<HashSet<&'static str>> = Lazy::new(|| {
     let keys = [
-        "User",
-        "Group",
-        "Device",
-        "Type",
-        "Options",
         "Copy",
+        "Device",
+        "Group",
         "Label",
+        "Options",
+        "Type",
+        "User",
     ];
 
     let mut set = HashSet::with_capacity(keys.len());
--- src/systemd_unit/constants.rs
+++ src/systemd_unit/constants.rs
@@ -1,4 +1,4 @@
 
-pub const UNIT_SECTION: &str = "Unit";
 pub const INSTALL_SECTION: &str = "Install";
 pub const SERVICE_SECTION: &str = "Service";
+pub const UNIT_SECTION: &str    = "Unit";
