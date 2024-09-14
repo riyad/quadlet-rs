a105019c42096de4b7dc3423b0a5576a6f25db3d
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2024-09-14 22:07:12 +0200

    Add research comment to logger

--- src/quadlet/logger.rs
+++ src/quadlet/logger.rs
@@ -35,6 +35,8 @@ impl KmsgLogger {
     }
 
     fn log(&self, record: &Record) {
+        // every now and then I thinks, wouldn't it by nice if we could use `format_args!()` here ...
+        // ... yes, it would ... but we can't: https://github.com/rust-lang/rust/issues/92698
         let msg = format!(
             "quadlet-rs-generator[{}]: {} - {}",
             process::id(),
